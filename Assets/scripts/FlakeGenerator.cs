f33cdc17af806782511241a0685b1c35536ba885
Andy Coenen <andy@patreon.com>
2016-12-15 17:54:57 -0800

    Add pledge data to snowflake

--- Assets/scripts/FlakeGenerator.cs
+++ Assets/scripts/FlakeGenerator.cs
@@ -2,15 +2,21 @@
 using System.Collections;
 
 public class FlakeGenerator : MonoBehaviour {
-	public GameObject flake;
+	public Flake flake;
 	public Transform flakeSpawn;
+
+	public PledgeManager pledgeManager;
 
 	void Update () 
 	{
 		if (Input.GetButtonDown("Jump"))
 		{
 			Vector3 position = new Vector3(Random.Range(-4.0f, 1.0f), 7, Random.Range(-13.0f, -19.0f));
-			Instantiate (flake, position, Random.rotation);
+			Flake newFlake = (Flake) Instantiate (flake, position, Random.rotation);
+
+			PledgeManager.Pledge pledge = pledgeManager.getPledge ();
+			newFlake.setPledge (pledge);
+
 		}
 	}
 }
---
+++ Assets/scripts/PledgeManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEngine.Networking;
+
+public class PledgeManager : MonoBehaviour {
+
+	private Pledge[] pledges;
+
+	public Pledge getPledge() {
+		return pledges [0];
+	}
+
+	public void test() {
+		print ("HELLS YEAH");
+	}
+
+	void Start() {
+		pledges = LoadPledges();
+	}
+
+	[System.Serializable]
+	public class PledgeList {
+		public Pledge[] pledges;
+	}
+
+	[System.Serializable]
+	public class Pledge {
+		public int amount_cents;
+		public int created_at;
+		public string creator_name;
+		public string creator_photo;
+		public string is_nsfw;
+		public string location_name;
+		public string patron_name;
+	}
+		
+	private Pledge[] LoadPledges() {
+		string toParse = File.ReadAllText (Application.dataPath + "/resources/pledges.json");
+
+		PledgeList pledgeList = JsonUtility.FromJson<PledgeList>(toParse);
+
+		Debug.Log (pledgeList.pledges.Length);
+
+		foreach (Pledge pledge in pledgeList.pledges) {
+			Debug.Log(pledge.creator_name);
+		}
+
+		return pledgeList.pledges;
+	}
+}
+
+
