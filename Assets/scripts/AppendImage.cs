398ebe8ad938e6423274f8f92bd9f33579f72a65
Spencer Handley <spencerhandley@gmail.com>
2016-12-15 17:05:08 -0800

    latest

---
+++ Assets/scripts/AppendImage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppendImage : MonoBehaviour {
+
+	public string url = "https://lh6.googleusercontent.com/-uuKimh_9DGI/AAAAAAAAAAI/AAAAAAAAAAA/VV0ipCD8g5s/s128-c-k/photo.jpg";
+
+	IEnumerator Start() {
+
+		Texture2D tex;
+		tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+		WWW www = new WWW(url);
+		yield return www;
+		www.LoadImageIntoTexture(tex);
+		GetComponent<Renderer>().material.mainTexture = tex;
+	}
+}
---
+++ Assets/scripts/FlakeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlakeGenerator : MonoBehaviour {
+	public GameObject flake;
+	public Transform flakeSpawn;
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+			Vector3 position = new Vector3(Random.Range(-4.0f, 1.0f), 7, Random.Range(-13.0f, -19.0f));
+			Instantiate (flake, position, Random.rotation);
+		}
+	}
+}
